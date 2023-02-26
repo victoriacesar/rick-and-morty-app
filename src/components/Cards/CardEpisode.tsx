c04bc15364a596a8195180a8c27654e4b4f36a21
Victória César <victoriacesaras@gmail.com>
2023-02-26 17:55:25 -0300

    feat(episode): page episode

---
+++ src/components/Cards/CardEpisode.tsx
@@ -0,0 +1,99 @@
+import {
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useQueries } from "@tanstack/react-query";
+import { scrollStyle } from "../../styles/globalTheme";
+import { fetchResident } from "../../utils/functions";
+import { Character } from "../../utils/interfaces";
+
+interface ICardEpisode {
+  name: string;
+  airDate: string;
+  episode: string;
+  characters: string[];
+}
+
+function CardEpisode({ name, airDate, characters, episode }: ICardEpisode) {
+  const results = useQueries({
+    queries: characters.map((character) => {
+      return {
+        queryKey: ["character", character],
+        queryFn: () => fetchResident(character),
+      };
+    }),
+  });
+
+  const characterInfo = results.map((character) => character.data);
+
+  const textColor = useColorModeValue("blackAlpha.800", "whiteAlpha.800");
+
+  return (
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      width="500px"
+      minWidth="450px"
+      height="285px"
+      bgColor={useColorModeValue("white", "gray.800")}
+    >
+      <Stack padding=".75rem">
+        <Flex flexDirection="column" gap="15px">
+          <Flex flexDirection="column">
+            <Heading fontSize="2xl" color={textColor}>
+              {name}
+            </Heading>
+          </Flex>
+          <Flex flexDirection="column">
+            <Text fontSize="14px" color="gray.500">
+              Air date:
+            </Text>
+            <Text color={textColor}>{airDate}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Text fontSize="14px" color="gray.500">
+              Episode:
+            </Text>
+            <Text color={textColor}>{episode}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Text fontSize="14px" color="gray.500">
+              Characters who participated in the episode:
+            </Text>
+            <Flex
+              wrap="wrap"
+              gap="8px"
+              overflowY="auto"
+              maxHeight="80px"
+              sx={scrollStyle}
+            >
+              {characterInfo.length &&
+                characterInfo?.map((elem: Character) => {
+                  return (
+                    <Box key={elem?.id} width="80px" height="80px">
+                      <Image
+                        objectFit="cover"
+                        borderRadius={10}
+                        src={elem?.image}
+                        alt={elem?.name}
+                        title={elem?.name}
+                        loading="lazy"
+                      />
+                    </Box>
+                  );
+                })}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Stack>
+    </Card>
+  );
+}
+
+export default CardEpisode;
--- src/pages/Episodes/index.tsx
+++ src/pages/Episodes/index.tsx
@@ -1,11 +1,80 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { useCallback, useEffect, useRef } from "react";
+import CardEpisode from "../../components/Cards/CardEpisode";
 import Header from "../../components/Header";
+import useRickAndMortyAPI from "../../hooks/useRickAndMortyAPI";
+import { Episode } from "../../utils/interfaces";
 
-function Episodes() {
+function Locations() {
+  const {
+    data,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useRickAndMortyAPI("episode");
+
+  const observerElem = useRef<HTMLDivElement>(null);
+
+  const handleObserver = useCallback(
+    (entries: any) => {
+      const [target] = entries;
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage],
+  );
+
+  useEffect(() => {
+    if (observerElem.current == null) return;
+
+    const element = observerElem?.current;
+    const option = { threshold: 0 };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+    observer.observe(element);
+    return () => {
+      observer.unobserve(element);
+    };
+  }, [fetchNextPage, hasNextPage, handleObserver]);
+
+  if (isLoading) {
+    return (
+      <Flex alignItems="center" justifyContent="center" margin="0px auto">
+        <Text>Carregando...</Text>
+      </Flex>
+    );
+  }
+
   return (
     <>
       <Header />
+      <Flex maxW="100vw" padding="2rem 1.5rem">
+        <Flex wrap="wrap" gap={15} alignItems="center" justifyContent="center">
+          {data.map((elem: Episode) => {
+            return (
+              <CardEpisode
+                key={elem.id}
+                airDate={elem.air_date}
+                characters={elem.characters}
+                episode={elem.episode}
+                name={elem.name}
+              />
+            );
+          })}
+        </Flex>
+      </Flex>
+      <Flex
+        ref={observerElem}
+        alignItems="center"
+        justifyContent="center"
+        paddingBottom="1rem"
+      >
+        {isFetchingNextPage && hasNextPage ? "Loading..." : "No search left"}
+      </Flex>
     </>
   );
 }
 
-export default Episodes;
+export default Locations;
