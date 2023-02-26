84a51b97630ba8b2f93a5e0442f844b7f1133a5c
Victória César <victoriacesaras@gmail.com>
2023-02-26 17:52:10 -0300

    style(scroll): add style to scroll

--- src/styles/globalTheme.ts
+++ src/styles/globalTheme.ts
@@ -1,6 +1,17 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 import { type GlobalStyleProps } from "@chakra-ui/theme-tools";
 
+export const scrollStyle = {
+  "&::-webkit-scrollbar": {
+    width: "5px",
+    borderRadius: "8px",
+    backgroundColor: `rgba(13, 8, 8, 0.05)`,
+  },
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: `rgba(0, 0, 0, 0.05)`,
+  },
+};
+
 const config: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: true,
